89dcfcfb5b936fe50979c997aedd40cb8490d23a
ne20002 <ne20002@gmx.ch>
2025-01-31 09:19:13 +0000

    Rewrite devcontainer similar to friendica docker setup

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -11,17 +11,26 @@ sudo apt-get update
 # VSCode debugger profile
 mkdir -p .vscode && cp .devcontainer/launch.json .vscode/launch.json
 
-envsubst < $workspaceFolder/.devcontainer/include/my.cnf > /home/vscode/.my.cnf
+envsubst < $workspaceFolder/.devcontainer/my.cnf > /home/vscode/.my.cnf
 
 # Make the workspace directory the docroot
 echo ">>> Symlink $DocumentRoot to $workspaceFolder"
 sudo rm -rf $DocumentRoot
 sudo ln -fs $workspaceFolder $DocumentRoot
+sudo chgrp -R www-data /var/www
+
+echo 'error_reporting=0' | sudo tee /usr/local/etc/php/conf.d/no-warn.ini
+
+# Create docker image files
+sudo mkdir -p /usr/src/friendica/config
+sudo cp $workspaceFolder/.devcontainer/docker/config/*.php /usr/src/friendica/config/
+sudo cp $workspaceFolder/VERSION /usr/src/friendica/
+
+sudo mkdir -p $workspaceFolder/addon
+bin/composer.phar --no-dev install
 
 # Set proper permissions
 sudo chown -R $developmentUser:www-data $workspaceFolder
 sudo chmod -R g=u $workspaceFolder
-
-echo 'error_reporting=0' | sudo tee /usr/local/etc/php/conf.d/no-warn.ini
 
 exit 0
